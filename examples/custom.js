f4202798c87adab2b9a0ed094666c302458f7d95
shizle <gastonnkh@gmail.com>
2016-04-03 15:22:55 +0100

    package update changes

--- examples/custom.js
+++ examples/custom.js
@@ -2,8 +2,14 @@ colors={
 	positive:"orange",
 	high:"green",
 	medium:"purple",
-	low:"lightblue",
-	negative:"black"
-	}
+	low:"brown",
+	negative:"black",
+	container:"grey"
+	};
 var viewer = require('expression-viewer');
 var next = new viewer({el: rootDiv,protein:'NX_P01308',colors:colors});
+
+document.getElementById("button").onclick=function(e){
+	console.log(e);
+	 next.init(document.getElementById("changer").value);
+	}
